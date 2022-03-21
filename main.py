36c267547e73191b258f03abc63d2b822c71d0ee
Vishnu Balaji <v2b2y2k@gmail.com>
2022-03-21 22:21:30 +0530

    trial-2

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from datetime import datetime as dt
+import string
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class sensors(BaseModel):
+    sensor_1:str
+    sensor_2:str
+
+class UltrasonicSensors(BaseModel):
+    date : str
+    local_time : str    # In IST
+    sensors : sensors
 
 @app.get("/")
 def message():
     print("---Log message --> Message function is executed---")
     return "Success 200"
+
+@app.post("/sensors")
+def sensor_status(sensors: UltrasonicSensors):
+    print(sensors)
